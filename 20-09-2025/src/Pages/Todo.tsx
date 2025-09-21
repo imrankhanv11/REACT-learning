c16f16764749041fcc2fe1501bd0f693e6559b46
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-09-21 21:21:08 +0530

    Add files via upload

--- 20-09-2025/src/Pages/Todo.tsx
+++ 20-09-2025/src/Pages/Todo.tsx
@@ -1,11 +1,64 @@
 import type React from "react";
+import { useCallback, useMemo } from "react";
+import UseLocalStorage from "../Hooks/UseLocalStorage";
+import '../CSS/Todo.css'
+import TodoItem from "../Components/TodoLists";
+import TodoCounter from "../Components/TodoCounter";
+import TodoForm from "../Components/TodoForm";
+import { toast } from "react-toastify";
 
+export type Todo = {
+    id: number,
+    todo: string,
+    isCompleted: boolean
+}
 
-const Todo : React.FC = () =>{
-    return(
+const Todo: React.FC = () => {
+
+    const [todos, setTodo] = UseLocalStorage<Todo[]>("todo", []);
+
+    // Add todo
+    const addTodo = useCallback((value: string) => {
+        const newTodo: Todo = {
+            id: Date.now(), todo: value, isCompleted: false
+        }
+        setTodo(prev => [...prev, newTodo]);
+
+        toast.success("Todo Added Sucessfully");
+
+    }, [setTodo]);
+
+    // Delete todo
+    const deleteTodo = useCallback((id: number) => {
+        setTodo((prev) => prev.filter((t) => t.id !== id));
+        toast.error("Todo Deleted");
+    }, [setTodo]);
+
+    // CheckBox
+    const toggleComplete = useCallback((id: number) => {
+        setTodo((prev) =>
+            prev.map((t) =>
+                t.id === id ? { ...t, isCompleted: !t.isCompleted } : t
+            )
+        );
+    }, [setTodo]);
+
+    // Counters
+    const completedCount = useMemo(() => todos.filter(t => t.isCompleted).length, [todos]);
+    const pendingCount = useMemo(() => todos.length - completedCount, [todos, completedCount]);
+
+    return (
         <div>
-            todo
+            <TodoForm addTodo={addTodo} />
+
+            <TodoCounter todoCompleted={completedCount} todoPending={pendingCount} />
+
+            {todos.map((item) => (
+                <TodoItem key={item.id} todo={item} deleteTodo={deleteTodo} toggleTodo={toggleComplete} />
+            ))}
+
         </div>
+
     )
 }
 
